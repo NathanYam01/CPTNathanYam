bbe47406da2893d198652057fe9b44d8ed43ff65
NathanYam01 <kalong.yam26@ycdsbk12.ca>
2025-05-30 11:00:17 -0400

    Printing the main menu

--- NathanBlackJack.java
+++ NathanBlackJack.java
@@ -4,9 +4,36 @@ import java.awt.image.BufferedImage;
 
 public class NathanBlackJack{
 	public static void main(String[] args){
+		//Name of the game and the size of the console
 		Console con = new Console("BlackJack",1280,720);
+		//Main Menu
+		//Imports and tells where the image goes
 		BufferedImage blackjacklogo = con.loadImage("BlackJacklogo.png");
-		con.drawImage(blackjacklogo ,100,100);
+		con.drawImage(blackjacklogo ,250,200);
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+		con.println("                                                       ");
+
+
+
+		//Main Menu Text
+		con.println("                                                            Welcome To Nathan's Blackjack!      ");
+		con.println("                                                                                                ");
+		con.println("                                                            Type To Choose                      ");
+		con.println("                                                            (P) Play                            ");
+		con.println("                                                            (L) LeaderBoard                     ");
+		con.println("                                                            (H) Help                            ");
+		con.println("                                                            (Q) Quit                            ");		
+		
+		//Prints the image
 		con.repaint();
 	}
 }
