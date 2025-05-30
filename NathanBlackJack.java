04a411d1feaf07bce17ef5e8bb7b55153201c4dc
NathanYam01 <kalong.yam26@ycdsbk12.ca>
2025-05-30 10:18:25 -0400

    Added code for the logo
    Added the code to allow the logo of my game to appear on screen

--- NathanBlackJack.java
+++ NathanBlackJack.java
@@ -1,7 +1,12 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class NathanBlackJack{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("BlackJack",1280,720);
+		BufferedImage blackjacklogo = con.loadImage("BlackJacklogo.png");
+		con.drawImage(blackjacklogo ,100,100);
+		con.repaint();
 	}
 }
