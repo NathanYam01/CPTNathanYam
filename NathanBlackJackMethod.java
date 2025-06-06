33649739a5d9a4657b881844c2b6e771f350bdde
NathanYam01 <kalong.yam26@ycdsbk12.ca>
2025-06-06 10:06:50 -0400

    making my method for the card names

--- NathanBlackJackMethod.java
+++ NathanBlackJackMethod.java
@@ -1,8 +1,9 @@
 import arc.*;
 
 public class NathanBlackJackMethod{
-	public static void main(String[] args){
-		Console con = new Console();
-		
+	public static String getCardName(int intCardValue, int intCardSuit) {
+			String[] strSuits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+			String[] strFaces = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+			return strFaces[intCardValue - 1] + " of " + strSuits[intCardSuit - 1];
 	}
 }
