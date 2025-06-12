0cb3e79aff9af8e865009d59ed335998510d3010
NathanYam01 <kalong.yam26@ycdsbk12.ca>
2025-06-12 10:00:32 -0400

    added getCardValue method
    added so that the kings,queens,jack were 10 and aces were 11 or 1

--- NathanBlackJackMethod.java
+++ NathanBlackJackMethod.java
@@ -6,4 +6,11 @@ public class NathanBlackJackMethod{
 			String[] strFaces = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 			return strFaces[intCardValue - 1] + " of " + strSuits[intCardSuit - 1];
 	}
+	
+	public static int getCardValue (int intValue){
+		if (intValue == 1) return 11;
+		else if (intValue >=11) return 10;
+		else return intValue;
+	}
+	
 }
