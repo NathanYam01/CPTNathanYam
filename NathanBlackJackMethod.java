3ef9d631f1e2cb56ecbe4d9707af41122423ba50
NathanYam01 <kalong.yam26@ycdsbk12.ca>
2025-06-12 10:05:44 -0400

    added leaderboard and the bubble sort
    created a fileio so that it would create a leaderboard.txt file to save all the players who played and their money and then had an output so that you could see it n the txt file and on the game. I also created the bubble sort so that it would make sure that whoever got higher would move up on the leaderboard

--- NathanBlackJackMethod.java
+++ NathanBlackJackMethod.java
@@ -24,4 +24,61 @@ public class NathanBlackJackMethod{
 		while(intSum < 21 && intAces-- > 0) intSum = intSum - 10;
 		return intSum;
 	}
+	public  static void displayLeaderboard(Console con){
+		TextInputFile leaderboardfile = new TextInputFile("leaderboard.txt");
+		con.println("Money Leaderboard:");
+		
+		while(leaderboardfile.eof() == false){
+			String strplayerName = leaderboardfile.readLine();
+			int intplayerMoney = leaderboardfile.readInt();
+			con.println(strplayerName + " - $" + intplayerMoney);
+		}
+		
+		leaderboardfile.close();
+	}
+	
+	public static void savetoleaderboard(String name, int money){
+		TextInputFile leaderboardFile = new TextInputFile("leaderboard.txt");
+		String[] strplayerNames = new String[100];
+		int[] intplayerMoney = new int[100];
+		int intplayercount = 0;
+		
+		while(leaderboardFile.eof() == false){
+			strplayerNames[intplayercount] = leaderboardFile.readLine();
+			intplayerMoney[intplayercount] = leaderboardFile.readInt();
+			intplayercount++;
+		}
+		
+		leaderboardFile.close();
+		
+		strplayerNames[intplayercount] = name;
+		intplayerMoney[intplayercount] = money;
+		intplayercount++;
+		
+		bubbleSort(strplayerNames, intplayerMoney, intplayercount);
+		
+		TextOutputFile leaderboardOutputFile = new TextOutputFile("leaderboard.txt", false);
+		for(int count = 0; count < intplayercount; count++){
+			leaderboardOutputFile.println(strplayerNames[count]);
+			leaderboardOutputFile.println(intplayerMoney[count]);
+		}
+		
+		leaderboardOutputFile.close();
+	}
+	
+	public static void bubbleSort(String [] strNames, int[] intMoney, int intSize){
+		for(int intOuter = 0; intOuter < intSize - 1; intOuter++){
+			for(int intInner = 0; intInner < intSize - 1 - intOuter; intInner++){
+				if(intMoney[intInner] < intMoney[intInner + 1]){
+					int intTempMoney = intMoney[intInner];
+					intMoney[intInner] = intMoney[intInner + 1];
+					intMoney[intInner + 1] = intTempMoney;
+					
+					String strTempName = strNames[intInner];
+					strNames[intInner] = strNames[intInner + 1];
+					strNames[intInner + 1] = strTempName;
+				}
+			}
+		}
+	}
 }
